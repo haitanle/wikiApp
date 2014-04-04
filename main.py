f5bf10faf0a4e33821b98addc25bd5a4966e1f9f
Hai-Tan Le <haitanle88@gmail.com>
2014-04-04 09:27:56 -0400

    set up jinja2 template environment

--- main.py
+++ main.py
@@ -1,31 +1,50 @@
 import webapp2
+import jinja2
+import os 
 
 
 
-class MainHandler(webapp2.RequestHandler):
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
+
+class Handler(webapp2.RequestHandler):
+	def write(self, *a , **kwargs):
+		self.response.out.write(*a, **kwargs)  
+
+	def render_str(self, template, **kwargs):
+		template = env.get_template(template)   
+		return template.render(**kwargs)  
+ 
+	def render(self, template, **kwargs):
+		self.write(self.render_str(template, **kwargs))
+
+
+
+class MainPage(Handler):
 	def get(self):
-		self.response.write("Welcome to askCharlotte99.com")
+		self.render("front.html")
 
-class Signup(MainHandler):
+class Signup(Handler):
 	def get(self):
 		self.render(form)
 
-class Login(MainHandler):
+class Login(Handler):
 	pass
 
-class Logout(MainHandler):
+class Logout(Handler):
 	pass
 
-class EditPage(MainHandler):
+class EditPage(Handler):
 	pass
 
-class WikiPage(MainHandler):
+class WikiPage(Handler):
 	pass 
 
 
 PAGE_RE = r'(/(?:[a-zA-Z0-9_-]+/?)*)'
 app = webapp2.WSGIApplication([
-	('/', MainHandler),
+	('/', MainPage),
 	('/signup', Signup),
 	('/login', Login),
 	('/logout', Logout),
